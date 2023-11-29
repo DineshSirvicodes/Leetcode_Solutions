0ca6ac490c8e20190f78b08d919521a2bf7d148b
DineshSirvicodes <112575008+DineshSirvicodes@users.noreply.github.com>
2023-11-29 19:54:17 +0530

    Create SetMatrixZero.cpp

---
+++ SetMatrixZero.cpp
@@ -0,0 +1,22 @@
+void setZeroes(vector<vector<int>>& matrix) {
+        int rowsize = matrix.size();
+        int colsize= matrix[0].size();
+        vector<int> row(rowsize,0);
+        vector<int> col(colsize,0);
+        for(int i = 0 ; i<rowsize ; i++){
+            for(int j = 0; j<colsize ; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        for(int i = 0; i<rowsize ; i++){
+            for(int j = 0; j<colsize ; j++){
+                if(row[i]==1 ||col[j]==1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        return;
+}
