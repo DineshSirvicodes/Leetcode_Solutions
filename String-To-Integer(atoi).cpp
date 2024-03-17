2f5dde8f95a45aa36ae0eebc75521d32af3217a5
DineshSirvicodes <112575008+DineshSirvicodes@users.noreply.github.com>
2024-03-17 21:23:26 +0530

    Update String-To-Integer(atoi).cpp

--- String-To-Integer(atoi).cpp
+++ String-To-Integer(atoi).cpp
@@ -1,5 +1,5 @@
-class Solution {
-public:
+class Solution {                                                                                
+public:                                                                                
     int myAtoi(string s) {
         int i = 0;
         int n = s.length();
@@ -29,3 +29,35 @@ public:
         return ans*sign;
     }
 };
+
+#include<bits/stdc++.h>
+int createAtoi(string s) {  
+    int n = s.length();
+    int i = 0;
+    long long ans = 0;
+    int sign = 1;
+    while(i<n && s[i] == ' '){
+        i++;
+    }
+    if(s[i] == '-'){
+        sign = -1;
+        i++;
+    }
+    else if(s[i] == '+') i++;
+    while(i<n){
+        if(s[i]>='0' && s[i]<='9'){
+            ans = ans*10+ (s[i]-'0');
+            if(ans > INT_MAX && sign == -1) return INT_MIN;
+            else if( ans> INT_MAX && sign == 1) return INT_MAX;
+            i++;
+        }
+        else{
+            break; // Exit the loop if a non-digit character is encountered
+        }     
+    }
+    ans = ans*sign; // Move this statement outside the loop
+    if(ans > INT_MAX) return INT_MAX;
+    else if(ans < INT_MIN) return INT_MIN;
+    return ans;
+}
+
